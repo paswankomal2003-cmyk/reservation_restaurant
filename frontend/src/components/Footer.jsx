325cee908955f3e2de3c2799dc7532312e7fe825
Komal Paswan <paswankomal2003@gmail.com>
2025-12-01 00:40:12 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">KOMAL PASWAN</div>
+          <div className="right">
+            <p>AB263/571, Sector 25, Jagatpura, Jaipur</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By KOMAL PASWAN</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
